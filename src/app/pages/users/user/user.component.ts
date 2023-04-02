4d8b069c09348df8f59fb90fc538d1bb4a32aa65
farzadjamshidi <farzadjamshidicom@yahoo.com>
2023-04-02 15:03:36 +0300

    Initial user component

--- src/app/pages/users/user/user.component.ts
+++ src/app/pages/users/user/user.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { User } from '../../../core/models/user.model';
+import { IUserRepo } from '../../../core/repository/interfaces/user.interface';
 
 @Component({
   selector: 'app-user',
@@ -6,6 +10,31 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./user.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserComponent {
+export class UserComponent
+{
+  model!: User;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    @Inject('IUserRepo') private userRepo: IUserRepo
+  )
+  {
+  }
+
+  async ngOnInit(): Promise<void>
+  {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id)
+    {
+      this.model = await firstValueFrom(this.userRepo.getById(id));
+    }
+  }
+
+  async deleteUser(): Promise<void>
+  {
+    await firstValueFrom(this.userRepo.delete(this.model.id));
+    this.router.navigate(['user/list']);
+  }
 }
