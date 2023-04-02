623cd9d240e0624e07a516e8322b33ccd38a37ac
farzadjamshidi <farzadjamshidicom@yahoo.com>
2023-04-02 15:03:25 +0300

    Initial user list component

--- src/app/pages/users/list/user-list.component.ts
+++ src/app/pages/users/list/user-list.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../../../core/models/user.model';
+import { IUserRepo } from '../../../core/repository/interfaces/user.interface';
 
 @Component({
   selector: 'app-user-list',
@@ -6,6 +10,24 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./user-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit
+{
+  list$!: Observable<User[]>;
 
+  constructor(
+    private router: Router,
+    @Inject('IUserRepo') private userRepo: IUserRepo
+  )
+  {
+  }
+
+  ngOnInit(): void
+  {
+    this.list$ = this.userRepo.getAll();
+  }
+
+  clickUser(user: User): void
+  {
+    this.router.navigate(['user/' + user.id]);
+  }
 }
