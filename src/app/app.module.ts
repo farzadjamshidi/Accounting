47a6128dd560db3fcc203d469dbe8e80fb4cdb5a
farzadjamshidi <farzadjamshidicom@yahoo.com>
2023-05-09 11:55:26 +0300

    Change button to material button in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+const MATERIAL_MODULES = [MatButtonModule];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ...MATERIAL_MODULES,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
